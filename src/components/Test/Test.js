cc23d47570bed70d719deea2fb478454572064e9
flomermer <tomer.flom@linearb.io>
2022-11-06 11:21:45 +0200

    Add sass

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
+import './Test.scss';
 import { useDispatch } from "react-redux";
-import { newUiStateAction } from "../state/actions/ui-state";
+import { newUiStateAction } from "../../state/actions/ui-state";
 
-const Test = () => {
+const Test = ({ className }) => {
     const dispatch = useDispatch();
 
     useEffect(() => {
         newUiStateAction({ keren: 'tomer' })(dispatch);
     }, [dispatch])
     return (
-        <div>
+        <div className={`Test ${className}`}>
             this is test component!
         </div>
     )
