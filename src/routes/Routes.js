b60b9acdf36af9844d1aa2478bd1e829bdd62bb4
“Keren <keren@linearb.io>
2022-11-06 17:10:11 +0200

    Responsive

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,22 +1,26 @@
-import { Switch, Router, Route } from 'react-router-dom';
-import _ from 'lodash';
-import AuthRoute from './AuthRoute';
-import history from './history';
-import ROUTES_MODEL from './routes.model';
+import { Switch, Router, Route } from "react-router-dom";
+import _ from "lodash";
+import AuthRoute from "./AuthRoute";
+import history from "./history";
+import ROUTES_MODEL from "./routes.model";
 
 const Routes = () => {
-    return (
-        <Router history={history}>
-            <Switch>
-                {_.map(ROUTES_MODEL, route => {
-                    const WhichRoute = route.isAuth ? AuthRoute : Route;
-                    return (
-                        <WhichRoute key={route.path} path={route.path} component={route.component} />
-                    );
-                })}
-            </Switch>
-        </Router>
-    )
-}
+  return (
+    <Router history={history}>
+      <Switch>
+        {_.map(ROUTES_MODEL, (route) => {
+          const WhichRoute = route.isAuth ? AuthRoute : Route;
+          return (
+            <WhichRoute
+              key={route.path}
+              path={route.path}
+              component={route.component}
+            />
+          );
+        })}
+      </Switch>
+    </Router>
+  );
+};
 
 export default Routes;
