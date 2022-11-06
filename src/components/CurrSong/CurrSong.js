953a04df005678bd46f4846402fe3879032fef96
“Keren <keren@linearb.io>
2022-11-07 01:07:21 +0200

    CurrSong

--- src/components/CurrSong/CurrSong.js
+++ src/components/CurrSong/CurrSong.js
@@ -1,7 +1,27 @@
 import "./CurrSong.scss";
 
 const CurrSong = ({ className }) => {
-  return <div className={`CurrSong ${className}`}></div>;
+  return (
+    <div className={`CurrSong ${className}`}>
+      <div className="CurrSongContent">
+        <div className="CurrSongImageAndDetails">
+          <div className="CurrSongImage"></div>
+          <div className="CurrSongDetails">
+            <div className="CurrSongName">Duck Duck Goose</div>
+            <div className="CurrSongArtist">CupcakKe</div>
+            <div className="CurrSongRating">
+              <div className="mid-gray"></div>
+              <div className="CurrSongUser"></div>
+              <div className="CurrSongAddedBy">Added by: Tomer</div>
+            </div>
+          </div>
+        </div>
+        <div className="progressBar">
+          <div className="progressBarFill" style={{ width: "10%" }}></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CurrSong;
