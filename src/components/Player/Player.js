78aa61435c1171f04ed535977299f4493324e5a4
Yeela Lifshitz <yeelalifshitz@MacBook-Pro.local>
2022-11-07 03:28:03 +0200

    player styles

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -4,7 +4,16 @@ const Player = () => {
   return (
     <div>
       <SpotifyPlayer
-        token='BQCqpQdvS1QSt5BUiLXtwANruQZMxgjAEO5IxZky99OELX-1j-ohLLvAwlBaXVSmssG7kya_MsdAkLdrKIk186-PnCbM2sbrEbQkgL5STjXYtX4nMF1wqQB6j7M25myNZtObtmDnXlPIk7800KYrMfI0WiP64bpNuZ0whJ6a34dFoMLKKoyOR-M42WzG8TB1gSM6ki40K-p3aqVm5Rg7WH-1S9ob7E6wmdYJ'
+        styles={{
+          activeColor: '#fff',
+          bgColor: '#222326',
+          color: '#fff',
+          loaderColor: '#fff',
+          sliderColor: '#1cb954',
+          trackArtistColor: '#ccc',
+          trackNameColor: '#fff'
+        }}
+        token='BQDJ_3AzxSOMgDDzJ77jnfZr2bLG8fPF1-wQ9NpklwschQlYghwyueAsI-cWEH9E7GhTTft6YjhLiHvljFw_xAh0frQw0YKtDF11Rfpoa23-P6bpaYQ2TXUltjdjcV1M87RnRL9-GNBF66WiL1KeN5PL_CfbqJEno3PhTruxJWthgRkKDHyus0HYTV1bwhRe60tGFHzTfr4xNkUo144yOC1VscmjvQq95HLi'
         uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
       />
       ;
