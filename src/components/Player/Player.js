0a64177ca41feff6abae978ed61fb9c04f1c3f64
Yeela Lifshitz <yeelalifshitz@MacBook-Pro.local>
2022-11-07 09:58:11 +0200

    get token button

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,47 +1,61 @@
-import { useState } from "react";
-import _ from "lodash";
-import SpotifyPlayer from "react-spotify-web-playback";
-import { useDispatch } from "react-redux";
-import { currSongFinished } from "../../state/actions/playlist";
+import { useState } from 'react';
+import _ from 'lodash';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import { useDispatch } from 'react-redux';
+import { currSongFinished } from '../../state/actions/playlist';
 
 const Player = ({ currSong, playlist }) => {
   const dispatch = useDispatch();
   const [token, setToken] = useState({
     access_token:
-      "BQAWdi7VJsfiZIAK09r0oPhnW5BTpPRJeZLohErME0gEa9c9Frm3IDA5bY69a40LWTUQRYHMP9XxgWevpe6sYG0d2UY-iEO-cPFCg-TUJVg6VWyNkVuJwdhqexv-qtYrsbW1jsv2l6bhAqf9y0cumv9KE-2pOXG4q210XR48A3W6oLRQOsgvP-TmJTOWPOCZUTk18dNP5ZEICmYEf79aUqoZjG7Q3zcsUHd9",
-    token_type: "Bearer",
+      'BQDDTSGOzy2unfW0sSbt2lHNDRCFIlEYFIytil7dK9E6yqiU0n01e1trRyFAjodSIUUYFlaJFUg3Ny7fFXCHxEd4tLmPF4QuyTX0WOh7PBwvS_Fr6zvV_lx73hrhtnLtFePQqbBsCIbLPedk8Lksa6LTslsi1dpN_WVSRLbisA8WNbtmPemX2slvBR46EaIp9SPmKZ53T5wn',
+    token_type: 'Bearer',
     expires_in: 3600,
     refresh_token:
-      "AQBkRmeUBcfewRDCQ32lhyRDi82OoSlb_6HrZFtrTJ4fB9Qkb81XvU0FElrZGOeDnnSmrb146HIyDr3sSSIlZOX3M3EgKPPsWtxxdRs0EJACNBz9oAZpJsuxB1vycBnYl9Y",
+      'AQANtoTwxi8uR5ywvbmUMKs8pyrA4g_LLtXLzSiRzAfkCfB5yGjbqJKJZxS_H09p-GbdKqyItYbfn4wp5Rr4EnpBOOVKIfIlDa-MV4nV84QCDrHIM7LoZYJqLjfROALpRdQ',
     scope:
-      "streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-read-email user-read-private user-top-read",
+      'streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-read-email user-read-private user-top-read'
   });
-  const currSongUri = _.get(currSong, "uri");
+  const currSongUri = _.get(currSong, 'uri');
   console.log({ currSong, currSongUri });
 
   const tracks = [currSongUri, ...playlist.map((song) => song.uri)];
-  console.log("flom", { tracks });
-  return (
+  console.log('flom', { tracks });
+  console.log('TOKEN: ', token);
+  return Object.keys(token).length === 0 ? (
+    <div>
+      <div className='token-not-available'>Audio player isn't avaliable, please generate spotify token</div>
+      <div
+        className='get-token-btn'
+        onClick={(e) => {
+          e.stopPropagation();
+          setToken('');
+        }}
+      >
+        Get Token For Spotify
+      </div>
+    </div>
+  ) : (
     <div>
       <SpotifyPlayer
         styles={{
-          activeColor: "white",
-          bgColor: "unset",
-          color: "white",
-          loaderColor: "#fff",
-          sliderColor: "#17E1BD",
-          sliderTrackColor: "#61656C",
-          sliderHandleColor: "#61656C",
-          trackArtistColor: "white",
-          trackNameColor: "white",
+          activeColor: 'white',
+          bgColor: 'unset',
+          color: 'white',
+          loaderColor: '#fff',
+          sliderColor: '#17E1BD',
+          sliderTrackColor: '#61656C',
+          sliderHandleColor: '#61656C',
+          trackArtistColor: 'white',
+          trackNameColor: 'white'
         }}
         token={token.access_token}
         uris={tracks}
         callback={(state) => {
-          const PlayerSongUri = _.get(state, "track.uri");
-          const nextTrackUri = _.get(state, "nextTracks[0].uri");
+          const PlayerSongUri = _.get(state, 'track.uri');
+          const nextTrackUri = _.get(state, 'nextTracks[0].uri');
           if (!!currSongUri && currSongUri !== PlayerSongUri) {
-            console.log("song-finished!");
+            console.log('song-finished!');
             return currSongFinished()(dispatch);
           }
         }}
