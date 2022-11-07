50b068321b0771dc2a84b9d7bb43eb5e696f421f
ShakedZrihen <shaked.zrihen@gmail.com>
2022-11-07 10:10:53 +0200

    Fixes

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import _ from "lodash";
-import SpotifyPlayer from "react-spotify-web-playback";
-import { useDispatch } from "react-redux";
-import { currSongStarted } from "../../state/actions/playlist";
+import { useEffect, useState } from 'react';
+import _ from 'lodash';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import { useDispatch } from 'react-redux';
+import { currSongStarted } from '../../state/actions/playlist';
 
 const Player = ({ playlist }) => {
   const dispatch = useDispatch();
   const [token, setToken] = useState({
     access_token:
-      "BQAWdi7VJsfiZIAK09r0oPhnW5BTpPRJeZLohErME0gEa9c9Frm3IDA5bY69a40LWTUQRYHMP9XxgWevpe6sYG0d2UY-iEO-cPFCg-TUJVg6VWyNkVuJwdhqexv-qtYrsbW1jsv2l6bhAqf9y0cumv9KE-2pOXG4q210XR48A3W6oLRQOsgvP-TmJTOWPOCZUTk18dNP5ZEICmYEf79aUqoZjG7Q3zcsUHd9",
-    token_type: "Bearer",
+      'BQAm8rYdaIFKriS2enJNQuuuBk6rBshMV7jvis_hF2g9AliLSiU4qDy037Y4ZgbQaunVViMg1djdTkcPDG0yQL84yyg_JO4YdxugpurAUUqD3cd3ssXG2bHQO_4kakcAgDK-CnfYUSyJUoMPsBZIF0QYIhsmLx2LxWbIcHo0UTsib3Xgou9-oYaxUj6EfLUnq7e7vxJ2t1S6tAlsg_ZlXK0VvK2c_WhZ5SGT',
+    token_type: 'Bearer',
     expires_in: 3600,
     refresh_token:
-      "AQBkRmeUBcfewRDCQ32lhyRDi82OoSlb_6HrZFtrTJ4fB9Qkb81XvU0FElrZGOeDnnSmrb146HIyDr3sSSIlZOX3M3EgKPPsWtxxdRs0EJACNBz9oAZpJsuxB1vycBnYl9Y",
+      'AQAylbevgXwQSI_azSdsKHcVobIxbQmWEKEsp6paGixoKTF1z6P5UFZS-Hc5nvxZWjQgF3NX31b17ONByViSIdtGSODgSegE3FKSFZLYSpmSJQXEuieQostm7makfEkry1k',
     scope:
-      "streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-read-email user-read-private user-top-read",
+      'streaming user-modify-playback-state user-read-playback-state user-read-currently-playing user-read-email user-read-private user-top-read'
   });
   // console.log({ playlist });
   const tracks = playlist.map((song) => song.uri);
@@ -23,23 +23,31 @@ const Player = ({ playlist }) => {
     <div>
       <SpotifyPlayer
         styles={{
-          activeColor: "white",
-          bgColor: "unset",
-          color: "white",
-          loaderColor: "#fff",
-          sliderColor: "#17E1BD",
-          sliderTrackColor: "#61656C",
-          sliderHandleColor: "#61656C",
-          trackArtistColor: "white",
-          trackNameColor: "white",
+          activeColor: 'white',
+          bgColor: 'unset',
+          color: 'white',
+          loaderColor: '#fff',
+          sliderColor: '#17E1BD',
+          sliderTrackColor: '#61656C',
+          sliderHandleColor: '#61656C',
+          trackArtistColor: 'white',
+          trackNameColor: 'white'
         }}
+        syncExternalDevice={true}
+        magnifySliderOnHover={true}
         token={token.access_token}
         uris={tracks}
         callback={(state) => {
           const type = _.get(state, 'type');
           const progressMs = _.get(state, 'progressMs');
-          if (type === 'track_update' && progressMs === 0) {
-            console.log('song-started', { type });
+          if (
+            type === 'track_update' &&
+            progressMs === 0 &&
+            state?.status === 'READY' &&
+            state?.isPlaying &&
+            state?.isActive
+          ) {
+            console.log('song-started', { type, state });
             currSongStarted()(dispatch);
           }
         }}
