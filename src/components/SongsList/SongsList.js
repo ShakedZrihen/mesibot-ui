e6bd2f0d7c788003ac91295f794e3184ea65b7ff
flomermer <tomer.flom@linearb.io>
2022-11-07 05:19:02 +0200

    add keys to SongsList

--- src/components/SongsList/SongsList.js
+++ src/components/SongsList/SongsList.js
@@ -71,30 +71,32 @@ const SongsList = ({ className }) => {
   return (
     <div className={`SongsList ${className}`}>
       <table>
-        {playlistData &&
-          playlistData.map(
-            ({ name, artist, image, priority, length = "3:13" }, i) => {
-              return (
-                <tr>
-                  <td className="songNumber">{i + 1}</td>
-                  <td>
-                    <div
-                      className="songImage"
-                      style={{ backgroundImage: `url(${image})` }}
-                    ></div>
-                  </td>
-                  <td className="songDetails">
-                    <div className="songName">{name}</div>
-                    <div className="songArtist">{artist}</div>
-                  </td>
-                  <td>
-                    <div className="songUser"></div>
-                  </td>
-                  <td className="songLength">{length}</td>
-                </tr>
-              );
-            }
-          )}
+        <tbody>
+          {playlistData &&
+            playlistData.map(
+              ({ name, artist, image, priority, length = "3:13" }, i) => {
+                return (
+                  <tr key={name}>
+                    <td className="songNumber">{i + 1}</td>
+                    <td>
+                      <div
+                        className="songImage"
+                        style={{ backgroundImage: `url(${image})` }}
+                      ></div>
+                    </td>
+                    <td className="songDetails">
+                      <div className="songName">{name}</div>
+                      <div className="songArtist">{artist}</div>
+                    </td>
+                    <td>
+                      <div className="songUser"></div>
+                    </td>
+                    <td className="songLength">{length}</td>
+                  </tr>
+                );
+              }
+            )}
+        </tbody>
         <tbody>
           {[...Array(10)].map((e, i) => {
             return (
