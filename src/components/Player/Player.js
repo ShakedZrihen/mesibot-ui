3b83b85ec29d43fa44e279e6370dddacbd024681
ShakedZrihen <shaked.zrihen@gmail.com>
2022-11-11 16:29:46 +0200

    usePlayer

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,79 +1,15 @@
-import { useEffect, useState } from 'react';
 import _ from 'lodash';
 import SpotifyPlayer from 'react-spotify-web-playback';
 import { useDispatch } from 'react-redux';
 import { currSongStarted } from '../../state/actions/playlist';
-import { useLocation } from 'react-router';
-import { LOCAL_STORAGE_KEYS } from '../../common/utils/utils';
 import { SERVICE_URL } from '../../common/utils/api.consts';
-import axios from 'axios';
+import usePlayer from './usePlayer';
 
 const Player = ({ currSong, playlist }) => {
   const dispatch = useDispatch();
-  const location = useLocation();
-  const [token, setToken] = useState({});
-  const [playing, setPlaying] = useState(currSong?.map(({ uri }) => uri));
-  const [orderedPlaylilst, setOrderedPlaylist] = useState(
-    [...(currSong || []), ...playlist].map((song) => song.uri)
-  );
-  console.log('player state', { token, playing, orderedPlaylilst });
-  useEffect(() => {
-    if (
-      (currSong && currSong?.[0]?.uri !== playing?.[0]?.uri) ||
-      (playlist?.length && !orderedPlaylilst.length)
-    ) {
-      setPlaying(currSong?.map(({ uri }) => uri));
-      setOrderedPlaylist(
-        [...(currSong || []), ...playlist].map((song) => song.uri)
-      );
-    }
-  }, [currSong, playlist]);
+  const playerState = usePlayer({ currSong, playlist });
 
-  useEffect(() => {
-    const getToken = async (code) => {
-      const { data: token, status } = await axios.get(
-        `${SERVICE_URL}/spotify/redirect?code=${code}`
-      );
-      if (status === 200) {
-        setToken(token);
-        localStorage.setItem('token', JSON.stringify(token));
-        localStorage.removeItem(LOCAL_STORAGE_KEYS.CODE);
-      }
-    };
-    const refreshToken = async (savedToken) => {
-      const { data: token, status } = await axios.post(
-        `${SERVICE_URL}/spotify/refresh`,
-        { token: savedToken }
-      );
-      if (status === 200) {
-        setToken({ ...savedToken, ...token });
-        localStorage.setItem(
-          'token',
-          JSON.stringify({ ...savedToken, ...token })
-        );
-        localStorage.removeItem(LOCAL_STORAGE_KEYS.CODE);
-      }
-    };
-    const tokenFromLocalStorage = localStorage.getItem('token');
-    if (tokenFromLocalStorage && !token.access_token) {
-      refreshToken(JSON.parse(tokenFromLocalStorage));
-      const intervalId = setInterval(() => {
-        refreshToken(JSON.parse(tokenFromLocalStorage));
-      }, [30000]);
-      return () => clearInterval(intervalId);
-    }
-    if (!token.access_token) {
-      const pathname = _.get(location, 'pathname');
-      localStorage.setItem(LOCAL_STORAGE_KEYS.LAST_PATHNAME, pathname);
-      const code = localStorage.getItem(LOCAL_STORAGE_KEYS.CODE);
-      if (code) {
-        getToken(code);
-        return;
-      }
-    }
-  }, []);
-
-  return Object.keys(token).length === 0 ? (
+  return Object.keys(playerState.token).length === 0 ? (
     <div>
       <div className="token-not-available">
         Audio player isn't avaliable, please generate spotify token
@@ -103,19 +39,15 @@ const Player = ({ currSong, playlist }) => {
           trackNameColor: 'white'
         }}
         magnifySliderOnHover={true}
-        token={token.access_token}
-        uris={_.uniq(orderedPlaylilst).filter(Boolean)}
+        token={playerState.token.access_token}
+        uris={_.uniq(playerState.orderedPlaylilst).filter(Boolean)}
         callback={(state) => {
           console.log('state', state);
-          if (state.error) {
-            setToken({});
-            return;
-          }
           const type = _.get(state, 'type');
-          if (type === 'track_update' && state?.track.uri !== currSong?.uri) {
-            console.log('song-started', { type, state });
-            currSongStarted()(dispatch);
-          }
+          // if (type === 'track_update' && state?.track.uri !== currSong?.uri) {
+          //   console.log('song-started', { type, state });
+          //   currSongStarted()(dispatch);
+          // }
         }}
       />
     </div>
